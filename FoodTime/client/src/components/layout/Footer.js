24614038ba04c79c58359be21759ad436d156d3f
mehulpachpol <65392580+mehulpachpol@users.noreply.github.com>
2022-07-14 23:43:04 +0530

    Add files via upload

---
+++ FoodTime/client/src/components/layout/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer grey darken-4">
+      <div className="container white-text">
+        <div className="left">
+          <h6>FoodTime</h6>
+        </div>
+        <div className="right">
+          <h6>
+            Follow Us{" "}
+            <a href="https://www.instagram.com/" target="_blank">
+              <span className="blue-text">Instagram</span>
+            </a>
+          </h6>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ FoodTime/server.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/userRoute");
+const restaurantRoute = require("./routes/restaurantRoute");
+const orderRoute = require("./routes/orderRoute");
+
+dotenv.config();
+const app = express();
+
+
+const DB = "mongodb+srv://qwerty:C0aePymhBJD57Vs8@cluster0.4kr2m.mongodb.net/?retryWrites=true&w=majority"
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to DB"));
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/res", restaurantRoute);
+app.use("/api/order", orderRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening from port ${port}`);
+});
